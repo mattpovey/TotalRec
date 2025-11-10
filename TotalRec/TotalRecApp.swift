7097fd8563c545a1e7acca85473a61d33d3d8f64
Matthew Povey - maximumeffort <matt.povey@gmail.com>
2025-11-10 19:45:45 +0100

    Initial Commit

---
+++ TotalRec/TotalRecApp.swift
@@ -0,0 +1,17 @@
+//
+//  TotalRecApp.swift
+//  TotalRec
+//
+//  Created by Matthew Povey on 10/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct TotalRecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
